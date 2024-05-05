4a9b48e752496b5b4f49fd1f4af033a7762d9268
cse18107 <cse18107@cemk.ac.in>
2024-05-05 10:34:17 +0530

    Feat: Completed create problem api

--- src/controllers/problem.controller.js
+++ src/controllers/problem.controller.js
@@ -1,14 +1,24 @@
-const {StatusCodes} = require('http-status-codes');
 const NotImplemented = require('../errors/notInplemented.error');
-const BadRequest = require('../errors/BadRequest.error');
+const {ProblemService} = require('../services');
+const {ProblemRepository} = require('../repositories');
+const { StatusCodes } = require('http-status-codes');
+
+const problemService = new ProblemService(new ProblemRepository());
 
 function  pingProblemController(req, res) {
     return res.json({message: 'Ping controller is up'})
 }
 
-function addProblem(req, res, next) {
+async function addProblem(req, res, next) {
   try {
-    throw new NotImplemented('addProblem');
+    console.log("incoming req body", req.body);
+    const newProblem = await problemService.createProblem(req.body);
+    return res.status(StatusCodes.CREATED).json({
+      success: true,
+      message: 'Successfully created a new problem',
+      error: {},
+      data: newProblem
+    })
   } catch (error) {
     next(error);
   }
